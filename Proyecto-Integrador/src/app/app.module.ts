78a036c3915839d07be72464fa1797e09cb0fcd8
tiburon99 <tivirolifranco@gmail.com>
2022-04-18 20:40:43 -0300

    Header

--- Proyecto-Integrador/src/app/app.module.ts
+++ Proyecto-Integrador/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroImageComponent } from './components/hero-image/hero-image.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialMediaBtnsComponent } from './components/social-media-btns/social-media-btns.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroImageComponent,
+    LogoAPComponent,
+    SocialMediaBtnsComponent
   ],
   imports: [
     BrowserModule,
