537b87de1461c0404c04fa4ae3b2306e066769e9
tiburon99 <tivirolifranco@gmail.com>
2022-04-26 18:46:35 -0300

    Portfolio services

---
+++ Proyecto-Integrador/src/app/servicios/portfolio.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PortfolioService {
+
+  constructor() { }
+
+  obtenerDatos(){
+    console.log("El servicio Portfolio está funcionando.")
+  }
+}
